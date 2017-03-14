e1f012f20d8ef87d5f56fe48f4d85b12dec38d75
monolok <antoinebertin@me.com>
2017-03-14 11:57:36 -0500

    basic Bet model set up

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,6 +1,7 @@
 class BetsController < ApplicationController
   def index
   	flash[:kraken] = HTTParty.get("https://api.kraken.com/0/public/Ticker?pair=XXBTZEUR")["result"]["XXBTZEUR"]["c"][0].to_f.round(2)
-  	
+  	@bets = Bet.all
+  	@bet_last = Bet.last
   end
 end
