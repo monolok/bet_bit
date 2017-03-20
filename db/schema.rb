bc0c47f2b99533a906ee66d49d35fc90a5edb5db
monolok <antoinebertin@me.com>
2017-03-20 17:37:35 -0500

    db for dvlp

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170320162746) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bets", force: :cascade do |t|
     t.decimal  "base_price"
     t.datetime "created_at", null: false
